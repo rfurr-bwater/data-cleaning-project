3981846fa8c2c63c9eb36f4f07ae98d11975af2e
Robert Furr <rfurr@eagles.bridgewater.edu>
2026-04-21 11:17:56 -0400

    Finish cleaning functions

--- ama_functions.py
+++ ama_functions.py
@@ -9,24 +9,61 @@ def clean_exit_year(year_str:str) -> None|int:
     result = re.findall(regex,year_str)
     if not result:
         return None
-    result = result[0]
-    if len(result) == 2:
-        result = "19"+result
-    return result
+    year = result[0]
+    if len(year) == 2:
+        year = "19"+year
+    if year[0:2] == "20":
+        year = "19"+year[2:4]
+    return int(year)
 
-def clean_dob_dod(date_str:str) -> tuple:
-    '''Takes in a date string and returns a tuple containing the birth and death years'''
+def clean_dob(date_str:str) -> int | None:
+    '''Takes in a date string and returns an int with the birth year'''
+    regex_dob = r'DOB\S*\s*\d{1,2}\/\d{1,2}\/(\d{2,4})'
+    regex_birth = r'Born\S*\s*\d{1,2}\/\d{1,2}\/(\d{2,4})'
+    regex_dmy = r'\d{2}\S+(\d{2})'
+    regex_my = r'DOB\S*\s*\S*\s*(\d{4})'
 
-    regex_death = r'Died\S\s*|DOD\S\s*'
-    regex_birth = r'DOB\S\s*'
-    regex_datestring = r'\d+'
+    # DOB
+    result = re.findall(regex_dob,date_str)
+    if result:
+        year = result[0]
+        if len(year) == 2:
+            year = "19"+year
+        if year[0:2] == "20":
+            year = "19"+year[2:4]
+        return int(year)
+    
+    # Born
+    result = re.findall(regex_birth,date_str)
+    if result:
+        year = result[0]
+        if len(year) == 2:
+            year = "19"+year[0]
+        if year[0:2] == "20":
+            year = "19"+year[2:4]
+        return int(year)
+    
+    # For cases like 28-Aug-30
+    result = re.findall(regex_dmy,date_str)
+    if result:
+        year = result[0]
+        if len(year) == 2:
+            year = '19'+year
+        return int(year)
+    
+    # DOB: May 1947
+    result = re.findall(regex_my,date_str)
+    if result:
+        return int(result[0])
 
-    birth_yr = None
-    death_yr = None
+    return None
 
-    result_death = re.findall(regex_death,date_str)
-    if result_death:
-        print(re.findall(r'\d+',date_str))
-    return (birth_yr,death_yr)
-
-clean_dob_dod("Died: 04/05/1953")
+if __name__ == "__main__":
+    # Test script
+    a = "Born: 04/18/1873"
+    b = "DOB:  08/03/25"
+    c = "28-Aug-30"
+    d = "DOB: October 1966"
+    ls = [a,b,c,d]
+    for l in ls:
+        print(clean_dob(l))
--- ama_program.py
+++ ama_program.py
@@ -18,12 +18,19 @@ if __name__ == "__main__":
         count = 0
         for row in reader:
             new_row = dict()
-            new_row['ID'] = row[3]
+            new_row['Id'] = int(row[3])
             new_row['Exit_Year'] = clean_exit_year(row[1])
             new_row['Last_Name'] = row[0]
-            entries.append(new_row)
+            new_row['Birth_Year'] = clean_dob(row[4])
+            # Age at exit year
+            if new_row['Birth_Year'] and new_row['Exit_Year']:
+                exit_age = new_row['Exit_Year'] - new_row['Birth_Year']
+                new_row['Age_at_Exit'] = exit_age
+                # Filter 7 to 24 exit age
+                if 7 <= exit_age <= 24:
+                    entries.append(new_row)
 
     with open('alumni_clean.csv', 'w', newline='') as new_file:
-        csv_writer = csv.DictWriter(new_file,fieldnames=['ID','Last_Name','Exit_Year'])
+        csv_writer = csv.DictWriter(new_file,fieldnames=['Id','Last_Name','Exit_Year','Birth_Year','Age_at_Exit'])
         csv_writer.writeheader()
         csv_writer.writerows(entries)
